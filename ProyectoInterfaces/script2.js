b3291534b94fac7c0dafbbe1b400c6bbc37b3486
JaimeDevMrF <166673607+JaimeDevMrF@users.noreply.github.com>
2024-11-21 15:39:48 -0500

    Add files via upload

--- ProyectoInterfaces/script2.js
+++ ProyectoInterfaces/script2.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const button = document.getElementById('toggleButton');  
-    const classesToToggle = ['body', 'header', 'footer', '.nombre_usuario_header', '.menu_perfil', '.nombre_usuario_menu', '.estado_usuario', '.buscador_menu', '.menu', 'a', '.inicio', '.inicio:hover', '.tutorias_inicio', '.eventoss', '.cursos', '.redes','.menu-button', '.menu-content', '.menu-button2', '.menu-content2', '.contenedor-email', 'p', '.email_mensaje', 'h1','.email_mensaje2','.logo'];
+    const classesToToggle = ['body', 'header', 'footer', '.nombre_usuario_header', '.menu_perfil', '.nombre_usuario_menu', '.estado_usuario', '.buscador_menu', '.menu', 'a', '.inicio', '.inicio:hover', '.tutorias_inicio', '.eventoss', '.cursos', '.redes','.menu-button', '.menu-content', '.menu-button2', '.menu-content2', '.contenedor-email', 'p', '.email_mensaje', 'h1','.email_mensaje2','.logo', '.ayuda'];
 
     button.addEventListener('click', function() {
       
